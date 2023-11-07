d4f5428fab4a46c6f0017eb9c0d6a9de59a33a53
mahabubulhasibshawon <mahabubulhasibshawon@gmail.com>
2023-11-07 22:14:55 +0600

    copy and paste performed

--- PracticeLocator/src/test/java/org/example/PracticeLocator/copyPaste.java
+++ PracticeLocator/src/test/java/org/example/PracticeLocator/copyPaste.java
@@ -1,11 +1,31 @@
 package org.example.PracticeLocator;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.testng.annotations.Test;
 
 public class copyPaste extends DriverSetup{
 	@Test
-	public void testCopyPaste() {
+	public void testCopyPaste() throws InterruptedException {
 		driver.get("https://rahulshettyacademy.com/AutomationPractice/");
+		Actions actions = new Actions(driver);
+		driver.findElement(By.xpath("//input[@id='autocomplete']")).sendKeys("Hello");
+		//pressing ctrl+a to select 
+		actions.keyDown(Keys.CONTROL);
+		actions.sendKeys("a").keyUp(Keys.CONTROL);
+		actions.build().perform();
+		Thread.sleep(5000);
 		
+		//pressing ctrl+c to select 
+		actions.keyDown(Keys.CONTROL);
+		actions.sendKeys("c").keyUp(Keys.CONTROL);
+		actions.build().perform();
+		Thread.sleep(5000);
+		
+		WebElement pastElement = driver.findElement(By.xpath("//input[@id='name']"));
+		pastElement.sendKeys(Keys.CONTROL, "v");
+		Thread.sleep(5000);
 	}
 }
