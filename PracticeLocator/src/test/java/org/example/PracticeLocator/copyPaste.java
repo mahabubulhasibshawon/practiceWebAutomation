0681aaf05fa3b8627f524d21a5e3067a26683385
mahabubulhasibshawon <mahabubulhasibshawon@gmail.com>
2023-11-07 12:57:12 +0600

    adding browser

--- PracticeLocator/src/test/java/org/example/PracticeLocator/copyPaste.java
+++ PracticeLocator/src/test/java/org/example/PracticeLocator/copyPaste.java
@@ -2,9 +2,10 @@ package org.example.PracticeLocator;
 
 import org.testng.annotations.Test;
 
-public class copyPaste {
+public class copyPaste extends DriverSetup{
 	@Test
 	public void testCopyPaste() {
+		driver.get("https://rahulshettyacademy.com/AutomationPractice/");
 		
 	}
 }
